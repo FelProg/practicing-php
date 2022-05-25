7d69bf7c7b0a46dfefe319bca0bfcb3dbc594bd9
FelProg <felixgerman@hotmail.com>
2022-05-24 22:37:11 -0700

    archivos introductorios php - echo - concatenación - comentarios

---
+++ imprimir/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imprimir</title>
+</head>
+<body>
+    
+    <h1>Realizando practicas con php -  Germán Félix</h1>
+    <?php
+        //comentario de codigo en php
+        echo '<h3>Listado de video juegos</h3>';
+        echo '<ul>'
+            .'<li>Street fighter</li>'
+            .'<li>Mario Bros.</li>'
+            .'<li>Fifa</li>'
+            .'</ul>';
+
+
+        /*comentario
+        multilinea*/
+        echo '<p>Esta es toda'.' - '.'la lista de juego'.'</p>';
+    ?>
+    
+</body>
+</html>
